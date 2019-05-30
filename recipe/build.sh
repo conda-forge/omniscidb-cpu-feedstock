bab9ecbce949bbf56c34a763a50e6285214a592a
Pearu Peterson <pearu.peterson@gmail.com>
2019-05-30 03:41:01 -0700

    Fixes and manual rebase against PR 4

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,10 +18,6 @@ $INPLACE_SED 's:DESTINATION ThirdParty/lib:DESTINATION lib:g' CMakeLists.txt
 
 export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
 
-# For debugging:
-# export CXXFLAGS="$CXXFLAGS -v -Wfatal-error"
-
-
 # Enforce PREFIX instead of BUILD_PREFIX:
 export ZLIB_ROOT=$PREFIX
 export LibArchive_ROOT=$PREFIX
@@ -61,70 +57,75 @@ if [ $(uname) == Darwin ]; then
     cat QueryEngine/CMakeLists.txt-orig >> QueryEngine/CMakeLists.txt
 
     $INPLACE_SED 's/ARGS -std=c++14/ARGS -std=c++14 -v --sysroot=\${BUILD_SYSROOT} -I\${BUILD_SYSROOT_INCLUDE}/g' QueryEngine/CMakeLists.txt
+
 else
     # Linux
     echo "uname=${uname}"
-    COMPILERNAME=clang                      # options: clang, gcc
-    GXX=$BUILD_PREFIX/bin/$HOST-g++         # replace with $GXX
-    GCCSYSROOT=$BUILD_PREFIX/$HOST/sysroot
-    GCCVERSION=$(basename $(dirname $($GXX -print-libgcc-file-name)))
-    GXXINCLUDEDIR=$BUILD_PREFIX/$HOST/include/c++/$GCCVERSION
-    GCCLIBDIR=$BUILD_PREFIX/lib/gcc/$HOST/$GCCVERSION
-
-    # Fix `not found include file` errors:
-    CXXINC1=$GXXINCLUDEDIR            # cassert, ...
-    CXXINC2=$GXXINCLUDEDIR/$HOST      # <string> requires bits/c++config.h
-    CXXINC3=$GCCSYSROOT/usr/include   # pthread.h
-
-    # Add include directories for explicit clang++ call in
-    # QueryEngine/CMakeLists.txt for building RuntimeFunctions.bc and
-    # ExtensionFunctions.ast:
-    mv QueryEngine/CMakeLists.txt QueryEngine/CMakeLists.txt-orig
-    echo -e "set(CXXINC1 \"-I$CXXINC1\")" > QueryEngine/CMakeLists.txt
-    echo -e "set(CXXINC2 \"-I$CXXINC2\")" >> QueryEngine/CMakeLists.txt
-    echo -e "set(CXXINC3 \"-I$CXXINC3\")" >> QueryEngine/CMakeLists.txt
-    cat QueryEngine/CMakeLists.txt-orig >> QueryEngine/CMakeLists.txt
-    $INPLACE_SED 's/ARGS -std=c++14/ARGS -std=c++14 \${CXXINC1} \${CXXINC2} \${CXXINC3}/g' QueryEngine/CMakeLists.txt
+    # must use gcc compiler as llvmdev is built with gcc and there
+    # exists ABI incompatibility between llvmdev-7 built with gcc and
+    # clang.
+    COMPILERNAME=gcc                      # options: clang, gcc
 
     if [ "$COMPILERNAME" == "clang" ]; then
+
+        GXX=$BUILD_PREFIX/bin/$HOST-g++         # replace with $GXX
+        GCCSYSROOT=$BUILD_PREFIX/$HOST/sysroot
+        GCCVERSION=$(basename $(dirname $($GXX -print-libgcc-file-name)))
+        GXXINCLUDEDIR=$BUILD_PREFIX/$HOST/include/c++/$GCCVERSION
+        GCCLIBDIR=$BUILD_PREFIX/lib/gcc/$HOST/$GCCVERSION
+
+        # Fix `not found include file` errors:
+        CXXINC1=$GXXINCLUDEDIR            # cassert, ...
+        CXXINC2=$GXXINCLUDEDIR/$HOST      # <string> requires bits/c++config.h
+        CXXINC3=$GCCSYSROOT/usr/include   # pthread.h
+
+        # Add include directories for explicit clang++ call in
+        # QueryEngine/CMakeLists.txt for building RuntimeFunctions.bc
+        # and ExtensionFunctions.ast:
+        mv QueryEngine/CMakeLists.txt QueryEngine/CMakeLists.txt-orig
+        echo -e "set(CXXINC1 \"-I$CXXINC1\")" > QueryEngine/CMakeLists.txt
+        echo -e "set(CXXINC2 \"-I$CXXINC2\")" >> QueryEngine/CMakeLists.txt
+        echo -e "set(CXXINC3 \"-I$CXXINC3\")" >> QueryEngine/CMakeLists.txt
+        cat QueryEngine/CMakeLists.txt-orig >> QueryEngine/CMakeLists.txt
+        $INPLACE_SED 's/ARGS -std=c++14/ARGS -std=c++14 \${CXXINC1} \${CXXINC2} \${CXXINC3}/g' QueryEngine/CMakeLists.txt
+
         export CC=$BUILD_PREFIX/bin/clang
         export CXX=$BUILD_PREFIX/bin/clang++
+        export CMAKE_CC=$BUILD_PREFIX/bin/clang
+        export CMKAE_CXX=$BUILD_PREFIX/bin/clang++
         export CXXFLAGS="$CXXFLAGS -I$CXXINC1 -I$CXXINC2 -I$CXXINC3"  # see CXXINC? above
         export CFLAGS="$CFLAGS -I$CXXINC3"                            # for pthread.h
-    else
-        # untested
-        # Note that go overwrites CC and CXX with nonsense (see
-        # https://github.com/conda-forge/go-feedstock/issues/47),
-        # hence we redefine these here:
-        export CC=$BUILD_PREFIX/bin/$HOST-gcc
-        export CXX=$BUILD_PREFIX/bin/$HOST-g++
-    fi
 
-    # resolves `cannot find -lgcc`:
-    export LDFLAGS="$LDFLAGS -Wl,-L$GCCLIBDIR"
+        # When using clang/clang++, make sure that linker finds gcc
+        # .o/.a files:
+        export CXXFLAGS="$CXXFLAGS  -B $GCCSYSROOT/usr/lib"  # resolves `cannot find crt1.o`
+        export CXXFLAGS="$CXXFLAGS  -B $GCCLIBDIR"           # resolves `cannot find crtbegin.o`
+        export CFLAGS="$CFLAGS  -B $GCCSYSROOT/usr/lib"      # resolves `cannot find crt1.o`
+        export CFLAGS="$CFLAGS  -B $GCCLIBDIR"               # resolves `cannot find crtbegin.o`
+
+        # resolves `cannot find -lgcc`:
+        export LDFLAGS="$LDFLAGS -Wl,-L$GCCLIBDIR"
+    else
+        export CC=$BUILD_PREFIX/bin/clang
+        export CXX=  # not used
+        export CMAKE_CC=$BUILD_PREFIX/bin/$HOST-gcc
+        export CMAKE_CXX=$BUILD_PREFIX/bin/$HOST-g++
+    fi
 
     # fixes `undefined reference to
     # `boost::system::detail::system_category_instance'` issue:
     export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
 
-    # When using clang/clang++, make sure that linker finds gcc .o/.a files:
-    export CXXFLAGS="$CXXFLAGS  -B $GCCSYSROOT/usr/lib"  # resolves `cannot find crt1.o`
-    export CXXFLAGS="$CXXFLAGS  -B $GCCLIBDIR"           # resolves `cannot find crtbegin.o`
-    export CFLAGS="$CFLAGS  -B $GCCSYSROOT/usr/lib"      # resolves `cannot find crt1.o`
-    export CFLAGS="$CFLAGS  -B $GCCLIBDIR"               # resolves `cannot find crtbegin.o`
-
     # make sure that $LD is always used for a linker:
     cp -v $LD $BUILD_PREFIX/bin/ld
 fi
 
-
-
-export CMAKE_COMPILERS="-DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX"  
+export CMAKE_COMPILERS="-DCMAKE_C_COMPILER=$CMAKE_CC -DCMAKE_CXX_COMPILER=$CMAKE_CXX"
 
 mkdir -p build
 cd build
 
-cmake \
+cmake -Wno-dev \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_BUILD_TYPE=release \
     -DMAPD_DOCS_DOWNLOAD=off \
@@ -140,6 +141,9 @@ cmake \
 
 make -j $CPU_COUNT
 make install
+
+# the latest double-conversion.so is installed to <prefix>/lib64:
+export LD_LIBRARY_PATH=$PREFIX/lib64:$LD_LIBRARY_PATH
 
 mkdir tmp
 $PREFIX/bin/initdb tmp
